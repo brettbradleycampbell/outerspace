f3cb0632af96085cecfbcb8511fee1e909411338
brettbradleycampbell <brettbradleycampbell@gmail.com>
2013-11-10 18:32:10 -0700

    Uploaded content
    Uploaded html, css, js, and image files.

---
+++ stack.js
@@ -0,0 +1,33 @@
+x = 0;  //horizontal coord
+y = document.height; //vertical coord
+window.scroll(x,y);
+
+
+var pane = $('#pane'),
+box = $('#box'),
+wh = pane.width() - box.width(),
+wv = pane.height() - box.height(),
+d = {},
+x = 5;
+
+function newh(v,a,b) {
+    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+    return n < 0 ? 0 : n > wh ? wh : n;
+}
+
+function newv(v,a,b) {
+    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+    return n < 0 ? 0 : n > wv ? wv : n;
+}
+
+$(window).keydown(function(e) { d[e.which] = true; });
+$(window).keyup(function(e) { d[e.which] = false; });
+
+setInterval(function() {
+    box.css({
+        left: function(i,v) { return newh(v, 37, 39); },
+        bottom: function(i,v) { return newv(v, 0, 38); } //disabled down key 0 = 40
+    });
+    wh = pane.width() - box.width();
+    wv = pane.height() - box.height();
+}, 20);
